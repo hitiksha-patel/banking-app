786d722af57da34e3a9a6d2b0ec848f264c42afb
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-13 17:26:05 -0400

    added user entity

---
+++ src/main/java/com/banking/banking_app/entity/User.java
@@ -0,0 +1,41 @@
+package com.banking.banking_app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstname;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastname;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phone;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Account> accounts;
+
+}
