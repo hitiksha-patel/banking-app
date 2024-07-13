9d97f2d79b058aa0c25d698b7eaee55142241605
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-13 17:33:04 -0400

    chnages in accountdto as user module added

--- src/main/java/com/banking/banking_app/dto/AccountDto.java
+++ src/main/java/com/banking/banking_app/dto/AccountDto.java
@@ -1,13 +1,20 @@
 package com.banking.banking_app.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class AccountDto {
 
     private Long id;
     private String accountHolderName;
-    private double balance;
+    private BigDecimal balance;
+    private Long userId;
 }
