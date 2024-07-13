3bb3c429fe501ac2ca41478f6d0b0b899d39fab2
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-13 17:28:23 -0400

    added userdto file

---
+++ src/main/java/com/banking/banking_app/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.banking.banking_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private List<Long> accountIds;
+    private String password;
+}
