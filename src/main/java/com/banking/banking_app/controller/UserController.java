0a351cd13a3a46d8f944c1fbd96171e4dbd8d357
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-14 00:10:15 -0400

    controller updates

--- src/main/java/com/banking/banking_app/controller/UserController.java
+++ src/main/java/com/banking/banking_app/controller/UserController.java
@@ -2,6 +2,7 @@ package com.banking.banking_app.controller;
 
 import com.banking.banking_app.dto.UserDto;
 import com.banking.banking_app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     public UserController(UserService userService){
@@ -23,4 +26,12 @@ public class UserController {
     public ResponseEntity<UserDto> addUser(@RequestBody UserDto userDto){
         return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto){
+        UserDto registeredUser = userService.registerUser(userDto);
+        return new ResponseEntity<>(registeredUser, HttpStatus.OK);
+    }
+
+
 }
