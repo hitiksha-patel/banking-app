5035004164f8d1b8f3b53b75b28270a21ac583d6
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-14 00:11:52 -0400

    impl file changes

--- src/main/java/com/banking/banking_app/service/impl/UserServiceImple.java
+++ src/main/java/com/banking/banking_app/service/impl/UserServiceImple.java
@@ -5,13 +5,17 @@ import com.banking.banking_app.entity.User;
 import com.banking.banking_app.mapper.UserMapper;
 import com.banking.banking_app.repository.UserRepository;
 import com.banking.banking_app.service.UserService;
+//import com.banking.banking_app.util.PasswordUtil;
+import com.banking.banking_app.util.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImple implements UserService {
 
+    @Autowired
     private UserRepository userRepository;
+
 
     @Autowired
     public UserServiceImple(UserRepository userRepository){
@@ -25,5 +29,11 @@ public class UserServiceImple implements UserService {
         return UserMapper.mapToUserDto(savedUser);
     }
 
-
+    @Override
+    public UserDto registerUser(UserDto userDto) {
+        User user = UserMapper.mapToUser(userDto);
+        user.setPassword(PasswordUtil.hashPassword(user.getPassword()));
+        User savedUser = userRepository.save(user);
+        return UserMapper.mapToUserDto(savedUser);
+    }
 }
