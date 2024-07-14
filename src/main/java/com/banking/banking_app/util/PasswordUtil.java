8d9adb39ba506cade76c9e1b8ec381f105b98892
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-14 00:13:05 -0400

    utils file

---
+++ src/main/java/com/banking/banking_app/util/PasswordUtil.java
@@ -0,0 +1,17 @@
+package com.banking.banking_app.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordUtil {
+
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public static String hashPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public static boolean checkPassword(String password, String hashedPassword) {
+        return passwordEncoder.matches(password, hashedPassword);
+    }
+}
