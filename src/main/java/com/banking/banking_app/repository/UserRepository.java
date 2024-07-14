5362d2c293b35e9d674f2814dc2eccbc09b5d675
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-14 00:10:56 -0400

    repository file chnages

--- src/main/java/com/banking/banking_app/repository/UserRepository.java
+++ src/main/java/com/banking/banking_app/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.banking.banking_app.repository;
 import com.banking.banking_app.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
