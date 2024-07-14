d80b8a62c103069662b8110adf00a120dfd30425
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-14 00:12:38 -0400

    configs added

---
+++ src/main/java/com/banking/banking_app/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.banking.banking_app.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/users/register").permitAll(); // Allow access to the register endpoint
+                    auth.anyRequest().authenticated(); // Require authentication for any other request
+                })
+                .csrf().disable() // Disable CSRF protection if not using form-based login
+                .formLogin().disable() // Disable form-based login
+                .httpBasic(); // Use basic HTTP authentication
+
+        return http.build();
+    }
+
+
+}
