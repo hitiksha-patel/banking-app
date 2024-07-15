460effe09e674be203630fc2e4411dd1c4086b8e
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-15 13:04:04 -0400

    security config changes

--- src/main/java/com/banking/banking_app/config/SecurityConfig.java
+++ src/main/java/com/banking/banking_app/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig{
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/users/register").permitAll(); // Allow access to the register endpoint
+                    auth.requestMatchers("/api/users/register", "/api/users/login").permitAll(); // Allow access to the register endpoint
                     auth.anyRequest().authenticated(); // Require authentication for any other request
                 })
                 .csrf().disable() // Disable CSRF protection if not using form-based login
