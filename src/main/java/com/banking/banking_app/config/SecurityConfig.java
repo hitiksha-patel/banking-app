a4d56c72bde9f6b0bace1ff93304252583be95b6
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-19 11:47:20 -0400

    security updated

--- src/main/java/com/banking/banking_app/config/SecurityConfig.java
+++ src/main/java/com/banking/banking_app/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.banking.banking_app.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,19 +32,20 @@ public class SecurityConfig {
         http
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/users/register", "/api/users/login", "/error").permitAll(); // Allow access to the register, login, and error endpoints
-                    auth.anyRequest().authenticated(); // Require authentication for any other request
+                    auth.requestMatchers("/api/users/register", "/api/users/login", "/error").permitAll();
+                    auth.requestMatchers(HttpMethod.PUT, "/api/users/**").authenticated(); // Ensure this line is present
+                    auth.anyRequest().authenticated();
                 })
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Use stateless session management
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable();
 
-        // Add the JWT filter before the UsernamePasswordAuthenticationFilter
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
