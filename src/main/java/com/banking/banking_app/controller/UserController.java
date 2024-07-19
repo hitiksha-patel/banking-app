ab2777463f83fb03ad64048b15eb404b67fec90f
hitiksha-patel <hitikshapatel23@gmail.com>
2024-07-19 14:38:31 -0400

    get user profile data api

--- src/main/java/com/banking/banking_app/controller/UserController.java
+++ src/main/java/com/banking/banking_app/controller/UserController.java
@@ -3,7 +3,10 @@ package com.banking.banking_app.controller;
 import com.banking.banking_app.dto.LoginDto;
 import com.banking.banking_app.dto.UserDto;
 import com.banking.banking_app.exception.UserAlreadyExistsException;
+import com.banking.banking_app.exception.UserNotFoundException;
 import com.banking.banking_app.service.UserService;
+import com.banking.banking_app.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     public UserController(UserService userService){
         this.userService = userService;
@@ -50,5 +56,37 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+            return ResponseEntity.ok("Successfully logged out");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No JWT token found in request headers");
+        }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserDto> getLoggedInUser(HttpServletRequest request) {
+        try {
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String jwt = authHeader.substring(7);
+                String username = jwtUtil.extractUsername(jwt);
+                UserDto userDto = userService.getUserByUsername(username);
+                return ResponseEntity.ok(userDto);
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+        } catch (UserNotFoundException e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            System.out.println("An error occurred while fetching the logged-in user's profile: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 
 }
