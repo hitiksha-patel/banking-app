7bc5566801f90e971159aece9ccfc13c32ed938b
hitiksha-patel <hitikshapatel23@gmail.com>
2024-08-12 17:06:30 -0400

    redis

--- src/main/java/com/banking/banking_app/BankingAppApplication.java
+++ src/main/java/com/banking/banking_app/BankingAppApplication.java
@@ -2,8 +2,10 @@ package com.banking.banking_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BankingAppApplication {
 
 	public static void main(String[] args) {
