44e9c01b0025afcb3a64a82cdab0327dfecff990
hitiksha-patel <hitikshapatel23@gmail.com>
2024-08-14 00:57:35 -0400

    user document feature

--- src/main/java/com/banking/banking_app/dto/UserDto.java
+++ src/main/java/com/banking/banking_app/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.Serializable;
 import java.util.List;
@@ -25,5 +26,9 @@ public class UserDto implements Serializable {
     private String password;
     private String token;
     private String role;
+    private String profilePic;
     private List<AccountDto> accounts;
+    private List<DocumentDto> documents;
+    private MultipartFile document;
+    private MultipartFile profilePicFile;
 }
